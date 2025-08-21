d5c53379428bdb20c74b7a11bd36b3b5c5e62aca
emotionallyUnstable <95876061+emotionallyUnstable@users.noreply.github.com>
2025-08-21 22:13:41 +0300

    initial commit 21.08.2025(slighty increase my task list)

--- TaskManager/src/Components/MyList.tsx
+++ TaskManager/src/Components/MyList.tsx
@@ -19,10 +19,14 @@ function MyList({ tasks }:MyListProps ) {
         <button className="border-1 py-[0.4em] px-[0.8em] rounded-4xl border-fuchsia-700 text-gray-500 hover:border-fuchsia-400 hover:text-white ml-[20px]">Active</button>
         <button className="border-1 py-[0.4em] px-[0.8em]  rounded-4xl border-fuchsia-700 text-gray-500 hover:border-fuchsia-400 hover:text-white">Completed</button>
        </div>
-       <div className="flex flex-col mt-[25px]">
+       <div className="flex flex-col mt-[25px] gap-3">
         {tasks.map((task,index) => (
-        <li key={index} className="flex h-[50px] w-[400px] justify-around gap-4 border-1">{task}
-        <button className="flex text-red-500">❌</button></li>
+
+          
+            <li key={index} className="flex h-[100px]  max-h-200px overflow-y-auto justify-around items-center gap-4 border-1"><span className="flex-1 max-w-[500px]   break-words">{task}</span>
+            <button className="flex text-red-500">❌</button></li>
+          
+
         ))}
        </div>
     </div>
